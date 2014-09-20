7743ba09845e1a9861c6e14b168fc7cf71370d15
CheapeOne <cheapeone@gmail.com>
2014-09-20 03:41:09 -0400

    Got PHP working

--- phpFiles/sendFunctions.php
+++ phpFiles/sendFunctions.php
@@ -5,37 +5,38 @@ include 'basicDB.php';
 $host = 'localhost';
 $username = root;
 $password = 'yuhclickyuh';
-$dbName;
-$user_table;
-$registered_user_table;
-$question_table;
+$dbName = 'acvdatabase';
+$user_table ='users';
+$registered_user_table = 'registeredusers';
+$question_table = 'questions';
 $answer_table;
 $user_answer;
 $user_post;
 
 
-function addQuestion($question_to_add) {
+function addQuestion($question_to_add, $content) {
 
 	global $host, $username, $password, $dbName, $user_table, $registered_user_table, $question_table;
 	global $answer_table, $user_answer, $user_post;
 
-	connectToDB($username, $password, $host, $dbName);
+
+	connectToDB($username, $password, $host, $dbName); // make sure this doesn't need to be called multiple times
 	
-	$countQuery = "SELECT COUNT(id) FROM $question_table";
-	$count = mysql_fetch_array(mysql_query($countQuery))[0];//we fetch an array of counts for each column and return the count of column 0
+	$countQuery = "SELECT COUNT(QID) FROM questions";
+	$count = mysql_fetch_array(mysql_query($countQuery))[0] ;//we fetch an array of counts for each column and return the count of column 0
 
-	$addQuestionQuery = "INSERT INTO $question_table VALUES ($count, $question_to_add)";
+	$addQuestionQuery = "INSERT INTO questions (QID, UID, title, _timestamp, numRating, content, category, location) VALUES ('$count', 7, '$question_to_add', 0, 0, '$content', 0, 0)";
 	$status = mysql_query($addQuestionQuery);
-
+	
 	if ($status == false) {// if the query failed, for whatever reason, let us know.
-
+		file_put_contents("out", mysql_error());
 		return false;
 	}
-
+	file_put_contents("out","true");
 	return true;
 }
 
-function addUser($new_username, $new_user_password) {
+function addUser($new_username, $new_user_password, $new_user_email) {
 
 	global $host, $username, $password, $dbName, $user_table, $registered_user_table, $question_table;
 	global $answer_table, $user_answer, $user_post;
@@ -45,7 +46,7 @@ function addUser($new_username, $new_user_password) {
 	$countQuery = "SELECT COUNT(id) FROM $registered_user_table";
 	$count = mysql_fetch_array(mysql_query($countQuery))[0];//we fetch an array of counts for each column and return the count of column 0
 
-	$addQuestionQuery = "INSERT INTO $registered_user_table VALUES ($new_username, $new_user_password)";
+	$addQuestionQuery = "INSERT INTO $user_table (UID, password, userType, sessionGeo, sessionIP) VALUES ('$new_username', '$new_user_password', 0, 0, 0)";
 	$status = mysql_query($addQuestionQuery);
 
 	if ($status == false) {// if the query failed, for whatever reason, let us know.
