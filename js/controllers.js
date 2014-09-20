8bbf582d96fa16c8c9ede9ae3ef01ea0fc42d0e1
CheapeOne <cheapeone@gmail.com>
2014-09-20 03:40:05 -0400

    Added base controllers

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,72 @@
 angular.module('ACVApp.controllers', []).
-  controller('homeController', function($scope) {
-  	$scope.homeMessage = "WELCOME YUH"
+  controller('homeController', function($scope, $http) {
+  	$scope.homeMessage = "WELCOME YUH";
+  	
+  	$scope.myAction = "addQuestion";
+  	$scope.myQuestionTitle;
+  	$scope.myQuestionBody;
+  	$scope.dbUrl = "phpFiles/sendToDB.php";
    
+  	$scope.postQuestion = function() {
+ 
+                var request = $http({
+                method: "post",
+                url: $scope.dbUrl,
+                params: {
+                	action: $scope.myAction,
+                	question_to_add: $scope.myQuestionTitle,
+                	question_body: $scope.myQuestionBody
+                },
+                data:  {
+                        question: $scope.question
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                });
+ 
+                /* Check whether the HTTP Request is Successfull or not. */
+                request.success(function (data) {
+                console.log("POST literally worked");
+                console.log($scope.question);
+                });
+        };
+
   }).
   controller('404Controller', function($scope) {
 
   	$scope.message = "NOTHING TO SEE HERE";
    
+  }).
+  controller('loginController', function($scope, $http) {
+
+  	$scope.loginMessage = "Signup Page";
+
+  	$scope.myAlias;
+  	$scope.myEmail;
+  	$scope.myPassword;
+  	$scope.dbUrl = "phpFiles/sendToDB.php";
+  	$scope.myAction = "addUser";
+
+   	$scope.addUser = function() {
+ 
+                var request = $http({
+                method: "post",
+                url: $scope.dbUrl,
+                params: {
+                	action: $scope.myAction,
+                	user_to_add: $scope.myAlias,
+                	email: $scope.myEmail,
+                	password: $scope.myPassword
+                },
+                data:  {
+                        question: $scope.question
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                });
+ 
+                /* Check whether the HTTP Request is Successfull or not. */
+                request.success(function (data) {
+                console.log("POST literally worked");
+                console.log($scope.question);
+                });
+        };
   });
