fad1ea01f44f8973a29a890cf73723618a6f1d01
CheapeOne <cheapeone@gmail.com>
2014-09-19 23:45:56 -0400

    Settup

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var ACVApp = angular.module('ACVApp', [
+  'ACVApp.controllers'
+]);
+
+/*
+.
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when("/404", 
+	{
+		templateUrl: "pages/404.html",
+	}).
+	when("/home",
+	{
+		templateUrl: "pages/home.html",
+	}).
+	otherwise(
+	{
+		redirectTo: '/404'
+	});
+}]);
+*/
---
+++ js/controllers.js
@@ -0,0 +1,5 @@
+angular.module('ACVApp.controllers', []).
+  controller('homeController', function($scope) {
+
+   
+  });
