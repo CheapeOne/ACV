7c7bb5172c00ed40db27469cee1f155b5106a7f9
CheapeOne <cheapeone@gmail.com>
2014-09-20 00:18:35 -0400

    Angular Setup COMPLETE

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('ACVApp.controllers', []).
   controller('homeController', function($scope) {
-
+  	$scope.homeMessage = "WELCOME YUH"
    
   }).
   controller('404Controller', function($scope) {
